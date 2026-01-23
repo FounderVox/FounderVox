e1372597f1d892c3dd60c912238ff62ad53be37e
Rehaan Hussain <rehaan@Rehaans-MacBook-Pro.local>
2026-01-23 11:22:44 +0300

    testing

--- lib/lemon-squeezy/server.ts
+++ lib/lemon-squeezy/server.ts
@@ -139,7 +139,7 @@ export async function createCheckoutUrl({
   }
 
   // Determine test mode from environment variable
-  const testMode = process.env.LEMON_SQUEEZY_TEST_MODE === "true"; 
+  const testMode = process.env.LEMON_SQUEEZY_TEST_MODE === "false"; 
   console.log('[createCheckoutUrl] Creating checkout with test_mode:', testMode);
 
   try {
